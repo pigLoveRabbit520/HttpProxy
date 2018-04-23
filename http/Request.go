9e72bb4db073f4e6689191d3e8b69fda3c33de5f
salamander-mh <1906747819@qq.com>
2018-04-23 18:04:56 +0800

    完成http Request的解析

--- http/Request.go
+++ http/Request.go
@@ -5,13 +5,19 @@ import (
 	"log"
 	"net"
 	"regexp"
+	"strconv"
 	"strings"
 )
 
 const (
-	BufferSize = 1024
-	CRLF       = "\r\n"
-	TWOCRLF    = "\r\n\r\n" // 两个CRLF
+	BufferSize               = 1024
+	LF                       = "\n"
+	CRLF                     = "\r\n"
+	TWOCRLF                  = "\r\n\r\n" // 两个CRLF
+	TWOLF                    = "\n\n"     // 两个LF
+	HEADER_CONTENT_LENGTH    = "content-length"
+	HEADER_TRANSFER_ENCODING = "transfer-encoding"
+	EMPTY_CHUNK              = "0\r\n\r\n"
 )
 
 // 请求类
@@ -29,25 +35,32 @@ func HandleClientRequest(client net.Conn) {
 	}
 	defer client.Close()
 
+	var buf [BufferSize]byte
 	request := Request{}
 	request.Headers = make(map[string]string)
-	var bufferStr = ""
+	readBuffer := make([]byte, 0)
+	bufferStr := ""
 	var headerEndPos int
+	bodyStartPos := 0
 
 	for {
-		var buf [BufferSize]byte
 		readBytesNum, err := client.Read(buf[:])
 		if err != nil {
 			log.Println("读取字节失败:" + err.Error())
 			return
 		}
-		bufferStr += string(buf[0:readBytesNum])
+		readBuffer = append(readBuffer, buf[:readBytesNum]...)
+		bufferStr = string(readBuffer)
 		if headerEndPos = strings.Index(bufferStr, TWOCRLF); headerEndPos >= 0 {
+			bodyStartPos = headerEndPos + len(TWOCRLF)
+			break
+		} else if headerEndPos = strings.Index(bufferStr, TWOLF); headerEndPos >= 0 { // 兼容性
+			bodyStartPos = headerEndPos + len(TWOLF)
 			break
 		}
 	}
-	headersStr := bufferStr[:headerEndPos+1]
-	lines := strings.Split(headersStr, CRLF)
+	startLineAndHeadersStr := bufferStr[:headerEndPos+1]
+	lines := regexp.MustCompile("\r\n|\n").Split(startLineAndHeadersStr, -1)
 	lineCount := len(lines)
 	for i := 0; i < lineCount; i++ {
 		var line = lines[i]
@@ -59,11 +72,77 @@ func HandleClientRequest(client net.Conn) {
 			request.Protocol = strs[2]
 		} else {
 			twoParts := strings.Split(line, ":")
+			fmt.Println("header is illegal: " + line)
+			if len(twoParts) != 2 {
+				return
+			}
 			headerName := strings.ToLower(strings.TrimSpace(twoParts[0]))
 			headerValue := strings.TrimSpace(twoParts[1])
 			request.Headers[headerName] = headerValue
 		}
 	}
-	request.Host = request.Headers["host"]
-	fmt.Printf("your request is %+v\n\n\n\n\n", request)
+
+	if host, ok := request.Headers["host"]; !ok {
+		fmt.Println("without host header")
+		return
+	} else {
+		request.Host = host
+	}
+	// transfer-encoding和content-length互斥
+	if transferEncoding, ok := request.Headers[HEADER_TRANSFER_ENCODING]; ok {
+		if transferEncoding == "chunked" {
+			bodyBuffer := make([]byte, 0)
+			// 可能还没读到body部分
+			if len(readBuffer)-1 > bodyStartPos {
+				bodyBuffer = readBuffer[bodyStartPos:]
+			}
+			// 判断最后5个字节
+			for len(bodyBuffer) < len(EMPTY_CHUNK) || string(bodyBuffer[len(bodyBuffer)-len(EMPTY_CHUNK):]) != EMPTY_CHUNK {
+				readBytesNum, err := client.Read(buf[:])
+				if err != nil {
+					log.Println("读取字节失败:" + err.Error())
+					return
+				}
+				bodyBuffer = append(bodyBuffer, buf[:readBytesNum]...)
+			}
+			readBuffer = append(readBuffer[:bodyStartPos], bodyBuffer...)
+		} else {
+			readBuffer = readBuffer[:bodyStartPos]
+		}
+	} else if contentLengthStr, ok := request.Headers[HEADER_CONTENT_LENGTH]; ok {
+		contentLength, err := strconv.Atoi(contentLengthStr)
+		if err != nil {
+			fmt.Println("content-length is illegal: " + contentLengthStr)
+			return
+		}
+		if contentLength > 0 {
+			bodyBuffer := make([]byte, 0)
+			// 可能还没读到body部分
+			if len(readBuffer)-1 > bodyStartPos {
+				bodyBuffer = readBuffer[bodyStartPos:]
+			}
+			for len(bodyBuffer) < contentLength {
+				readBytesNum, err := client.Read(buf[:])
+				if err != nil {
+					log.Println("读取字节失败:" + err.Error())
+					return
+				}
+				bodyBuffer = append(bodyBuffer, buf[:readBytesNum]...)
+			}
+			readBuffer = append(readBuffer[:bodyStartPos], bodyBuffer...)
+		} else {
+			readBuffer = readBuffer[:bodyStartPos]
+		}
+	} else {
+		readBuffer = readBuffer[:bodyStartPos]
+	}
+}
+
+func requestServer(host string, clientData []byte) ([]byte, error) {
+	serverSocket, err := net.Dial("tcp", host)
+	if err != nil {
+		return nil, err
+	}
+	defer serverSocket.Close()
+
 }
